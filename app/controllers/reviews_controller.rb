a98f17be64c3d9347c92a94e4a63fa33c6fbbdd1
Sebastian Pont Verges <sebaspont@gmail.com>
2023-08-16 21:47:56 +0200

    yelp mvp

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,29 +7,29 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
-    # else
-    #   render :new, status: :unprocessable_entity
-    # end
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render "restaurants/show", status: :unprocessable_entity
+    end
   end
 
-  # def destroy
-  #   @review = Review.find(params[:id])
-  #   @review.destroy
-  #   redirect_to restaurant_path(@review.restaurant), status: :see_other
-  # end
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant), status: :see_other
+  end
 
   private
 
-  # def set_restaurant
-  #   @restaurant = Restaurant.find(params[:restaurant_id])
-  # end
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 
   def review_params
-    params.require(:review).permit(:content, :rating)
+    params.require(:review).permit(:rating, :content)
   end
 end
